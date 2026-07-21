21e07fd92630fc900cfab961522b882269966007
aliyu12333 <aliyufaruk482@gmail.com>
2026-07-21 09:28:57 +0100

    Add files via upload

---
+++ day4 user input.py
@@ -0,0 +1,7 @@
+name = input("what is your name: ")
+age = int(input(f"how old are you {name} "))
+age = age + 1
+
+print(f"hello there {name} how is it going today ")
+print(f"{name} you are going to be {age} years old")
+
