f5d2294a8c817207656aa88e40e64a6798721d11
aliyu12333 <aliyufaruk482@gmail.com>
2026-07-22 20:13:46 +0100

    Add files via upload

---
+++ python everyday/day4 extras madlib.py
@@ -0,0 +1,13 @@
+#mad libs using user input
+
+Adjective = input("enter an adjective: ")
+noun = input("enter a noun: ")
+number = input("enter a number:")
+plural_noun = input("enter a noun plural noun: ")
+
+
+
+print(f"It was a {Adjective} morning")
+print(f"when I woke up and discovered a talking {noun}")
+print(f"in my closet! It was holding a map with{number}")
+print(f"{plural_noun} marked on it.")
