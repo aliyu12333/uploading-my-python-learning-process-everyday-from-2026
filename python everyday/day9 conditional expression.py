b61106c7f1c1cba7ac15c704b8c973db6168edaa
aliyu12333 <aliyufaruk482@gmail.com>
2026-07-30 17:21:54 +0100

    Add files via upload

---
+++ python everyday/day9 conditional expression.py
@@ -0,0 +1,23 @@
+#conditional expression = a one line shortcut for the if else statement
+#                       print or assign based on two values based on a condition
+#                       x if condition else y
+
+#num = -5
+#print("negative" if num < 0 else "positive")
+
+# num = 51
+# result = "EVEN" if num % 2 else "ODD"
+# print(result)
+
+# a = 6
+# b = 7
+#
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# print(min_num)
+
+
+age = 17
+
+status = "adult" if age >= 18 else "child"
+print(status)
---
+++ python everyday/day9 extras.py
@@ -0,0 +1,17 @@
+# # Asks the user for their score.
+# # If the score is 50 or more, print "PASS".
+# # Otherwise print "FAIL".
+#
+# # score = int(input("what is your score: "))
+# # result = "PASS" if score >= 50 else "FAIL"
+# # print(result)
+#
+#
+#
+#
+# Asks the user for their age.
+# Prints "You can vote" if the age is 18 or above.
+# Prints "You cannot vote" otherwise
+
+age = int(input("HOW OLD ARE YOU: "))
+print("You can vote" if age >= 18 else "Go home you cannot vote")
